0d1dc0b3a4341a16169b98b375adc54e4abce6e4
Jerry Leung <tingfung.leung@solarwinds.com>
2026-01-29 16:34:57 -0800

    nits

--- ext/cache_c_wrapper.h
+++ ext/cache_c_wrapper.h
@@ -8,21 +8,40 @@ extern "C" {
 #include <Zend/zend.h>
 
 /**
- * Allocate a Solarwinds::Cache object
- * @return void*
+ * Allocate and initialize a new cache instance.
+ *
+ * @param cache_max_entries The maximum number of entries the cache can hold.
+ * @return A pointer to the allocated cache instance.
  */
 void *Cache_Allocate(long cache_max_entries);
 
 /**
- * Free a Solarwinds::Cache object
- * @param cache void*
- * @return
+ * Free the allocated cache instance.
+ *
+ * @param cache A pointer to the cache instance to be freed.
  */
 void Cache_Free(void *cache);
 
+/**
+ * Store settings in the cache.
+ *
+ * @param cache A pointer to the cache instance.
+ * @param collector The collector endpoint.
+ * @param token The token.
+ * @param serviceName The name of the service.
+ * @param settings The settings to be cached.
+ */
 void Cache_Put(void *cache, char *collector, char *token, char *serviceName,
                char *settings);
 
+/**
+ * Retrieve settings from the cache.
+ * @param cache A pointer to the cache instance.
+ * @param collector The collector endpoint.
+ * @param token The token.
+ * @param serviceName The name of the service.
+ * @return The cached settings if found, otherwise NULL.
+ */
 zend_string *Cache_Get(void *cache, char *collector, char *token,
                        char *serviceName);
 
